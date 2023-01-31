3771f3858cf0b90e4bff3b2cd4090cce2aa4724d
Gege-0330 <108968591+Gege-0330@users.noreply.github.com>
2023-01-30 21:33:48 -0800

    Amanda1commit (#3)

---
+++ HW3_cli.py
@@ -0,0 +1,19 @@
+""" CLI program to for HW3 homework """
+# Put your name here
+import argparse
+
+
+if __name__ == "__main__":
+    # Set up argparse parser and arguments here
+    parser = argparse.ArgumentParser()
+    # Add the arguments here with parser.add_argument
+
+    args = parser.parse_args()
+    # Based on the argument information in the args variable,
+    # execute whatever the flags say to do here in the correct order.
+
+    # If you prefer to call a single main() function written above the
+    # if __name__ == "__main__":
+    # statement, that is OK with me.
+    # The important thing is to make it work correctly. ;-)
+    # But don't overthink things.
